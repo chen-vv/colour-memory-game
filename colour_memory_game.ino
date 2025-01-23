794abbb313159456d2f76ad747709502382d301e
chen-vv <vchen03@student.ubc.ca>
2025-01-23 13:01:47 -0800

    Game logic works with two colours

--- colour_memory_game.ino
+++ colour_memory_game.ino
@@ -8,6 +8,15 @@
 #define YELLOW_BUTTON_PIN 6
 #define GREEN_BUTTON_PIN 8
 
+unsigned int blinkTime;  // TODO: COMMENTS
+unsigned int pauseTime;
+unsigned int level;  // Starts from 0
+unsigned int sequence[50] = { 0 };
+unsigned int colours[4] = { RED_LED_PIN, BLUE_LED_PIN, YELLOW_LED_PIN, GREEN_LED_PIN };
+bool gameStarted = false;
+unsigned int currentColourLED;
+bool playerLost;
+
 void setup() {
   Serial.begin(9600);
 
@@ -20,30 +29,135 @@ void setup() {
   pinMode(BLUE_BUTTON_PIN, INPUT);
   pinMode(YELLOW_BUTTON_PIN, INPUT);
   pinMode(GREEN_BUTTON_PIN, INPUT);
+
+  randomSeed(analogRead(A5));
+
+  initializeGame();
 }
 
 void loop() {
-  if (digitalRead(RED_BUTTON_PIN) == LOW) {
-    digitalWrite(RED_LED_PIN, HIGH);
-  } else {
-    digitalWrite(RED_LED_PIN, LOW);
+  // if (gameStarted == false) {
+  //   if (digitalRead(GREEN_BUTTON_PIN) == LOW) {
+  //     digitalWrite(GREEN_LED_PIN, HIGH);
+  //     delay(1000);
+  //   }
+
+  //   digitalWrite(GREEN_LED_PIN, LOW);
+
+  //   Serial.println("Press all four buttons to start");
+
+  //   while (allButtonsPressed() == false) {}
+
+  //   gameStarted = true;
+
+  //   Serial.println("Starting...");
+  //   delay(2000);
+  // }
+
+  delay(1000);
+
+  level++;
+
+  for (int i = 0; i < level; i++) {
+    currentColourLED = sequence[i];
+
+    digitalWrite(currentColourLED, HIGH);
+    delay(blinkTime);
+
+    digitalWrite(currentColourLED, LOW);
+
+    if (i < level - 1) {
+      delay(pauseTime);
+    }
   }
 
-  if (digitalRead(BLUE_BUTTON_PIN) == LOW) {
-    digitalWrite(BLUE_LED_PIN, HIGH);
-  } else {
-    digitalWrite(BLUE_LED_PIN, LOW);
+  int count = 0;
+
+  while (count < level) {
+    unsigned int currentColourButton = sequence[count] - 1;
+
+    if (digitalRead(RED_BUTTON_PIN) == LOW) {
+      while (digitalRead(RED_BUTTON_PIN) == LOW) {
+        Serial.print("red");
+      }
+
+      Serial.println("Pressed red");
+      if (currentColourButton == RED_BUTTON_PIN) {
+        // Add point
+        count++;
+      } else {
+        // you lose
+        Serial.println("You lose!");
+        playerLost = true;
+        break;
+      }
+    }
+
+    else if (digitalRead(BLUE_BUTTON_PIN) == LOW) {
+      while (digitalRead(BLUE_BUTTON_PIN) == LOW) { Serial.print("blue"); }
+
+      Serial.println("Pressed blue");
+      if (currentColourButton == BLUE_BUTTON_PIN) {
+        // Add point
+        count++;
+      } else {
+        // you lose
+        Serial.println("You lose!");
+        playerLost = true;
+        break;
+      }
+    }
+
+    // if (digitalRead(YELLOW_BUTTON_PIN) == LOW) {
+    //   Serial.println("Pressed yellow");
+    //   if (currentColourButton == YELLOW_BUTTON_PIN) {
+    //     // Add point
+    //     count++;
+    //   } else {
+    //     // you lose
+    //     Serial.println("You lose!");
+    //     playerLost = true;
+    //     break;
+    //   }
+    // }
+
+    // if (digitalRead(GREEN_BUTTON_PIN) == LOW) {
+    //   Serial.println("Pressed green");
+    //   if (currentColourButton == GREEN_BUTTON_PIN) {
+    //     // Add point
+    //     count++;
+    //   } else {
+    //     // you lose
+    //     Serial.println("You lose!");
+    //     playerLost = true;
+    //     break;
+    //   }
+    // }
   }
 
-  if (digitalRead(YELLOW_BUTTON_PIN) == LOW) {
-    digitalWrite(YELLOW_LED_PIN, HIGH);
+  if (playerLost) {
+    gameStarted = false;
+    initializeGame();
   } else {
-    digitalWrite(YELLOW_LED_PIN, LOW);
+    // Add next colour - put this in function to use in init too
+    unsigned int nextColour = colours[random(0, 2)];
+    sequence[level] = nextColour;
   }
+}
 
-  if (digitalRead(GREEN_BUTTON_PIN) == LOW) {
-    digitalWrite(GREEN_LED_PIN, HIGH);
-  } else {
-    digitalWrite(GREEN_LED_PIN, LOW);
-  }
+void initializeGame() {
+  blinkTime = 1000;
+  pauseTime = 800;
+  level = 0;
+  playerLost = false;
+  unsigned int nextColour = colours[random(0, 2)];
+  sequence[level] = nextColour;
+}
+
+bool noButtonsPressed() {
+  return digitalRead(RED_BUTTON_PIN) == HIGH && digitalRead(BLUE_BUTTON_PIN) == HIGH && digitalRead(YELLOW_BUTTON_PIN) == HIGH && digitalRead(GREEN_BUTTON_PIN) == HIGH;
+}
+// TODO: COMMENTS
+bool allButtonsPressed() {
+  return digitalRead(RED_BUTTON_PIN) == LOW && digitalRead(BLUE_BUTTON_PIN) == LOW && digitalRead(YELLOW_BUTTON_PIN) == LOW && digitalRead(GREEN_BUTTON_PIN) == LOW;
 }
